b0c83be9df4f9826e56319f6171f506b33380f9b
Leonid <lenagovorec58@gmail.com>
2023-12-31 01:25:05 +0300

    Styles for the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import "./app.scss";
 
 export default function RouterComponent() {
     return (
-        <>
+        <div className="wrapper">
             <a href={`/form`}>FORM</a>
-        </>
+        </div>
     )
 }
