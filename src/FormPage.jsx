401a78be51e859beb7968c6a0b78011a92e0d71a
Leonid <lenagovorec58@gmail.com>
2023-12-31 13:31:59 +0300

    New changes

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import "./formPage.scss";
+
 export default function FormPage(){
+    const [input, setInput] = useState(0);
+    // const [textarea, setTextarea] = useState(0);
+    // const [checkbox, setCheckbox] = useState(0);
+    // const [show, setShow] = useState(false);
+    const [inputValues, setInputValues] = useState(Array(input).fill(''));
+    
+ const renderInputs = () => {
+    const inputs = inputValues.map((value, index) => (
+      <input key={index} type="text" value={value} />
+    ));
+    return <div>{inputs}</div>; 
+  };
     return(
-        <form>
-            <label htmlFor="">Привет это форма</label>
+        <form className="formSpace">
+            <label htmlFor="input">Input {input}</label>
+            <input type="number" name="input" min="0" onChange={e => setInput(parseInt(e.target.value))} />
+            {/* <label htmlFor="textarea">Textarea</label>
+            <input type="number" name="textarea"  onChange={e => setTextarea(e.target.value)} />
+            <label htmlFor="checkbox">Checkbox</label>
+            <input type="number" name="checkbox"  onChange={e => setCheckbox(e.target.value)} /> */}
+            <input type="button" value="Build" onClick={() => setInputValues(Array(input).fill(''))} />
+            {renderInputs()}
         </form>
+
     )
 }
